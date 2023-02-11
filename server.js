b1c3ce3aeaa63a8f63e1a39af53f9a443ca834e0
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-11 15:47:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,4 @@ const app = express();
 
 app.use(express.static(__dirname+'/'));
 
-const CsbInspector = require('csb-inspector/express-socket');
-const options = {
-     app: app,
-     route: "_console",
-     port: 8888,
-     outputs: [(path, prop, args)=>{
-           fs.appendFileSync("file.txt", path);
-       }]
-   }
-CsbInspector(options);
 app.listen(8080);
