ce58ce3b42630d882b3552dbbd59ba993278abba
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-11 15:22:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/'));
 
-console.log('escuchando en el puerto 8080');
+console.log('msg');
 app.listen(8080);
